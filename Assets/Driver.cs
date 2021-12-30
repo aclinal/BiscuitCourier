fef4901cdabc64567b8a5f60f018db841f4dc6dd
Alex Leung <alex.leung@outlook.com>
2021-12-30 12:24:44 -0500

    Implement WoofEx and a Driver script

---
+++ Assets/Driver.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = 300.0f;
+    [SerializeField] float moveSpeed = 20.0f;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float speedAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, speedAmount, 0);
+    }
+}
